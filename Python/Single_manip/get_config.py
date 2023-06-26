a3aa572a3459c6fe8c4887035185ef59c3539dd2
Nabil Ech-chokhamny <nabilech01@gmail.com>
2023-06-26 12:40:00 +0200

    commit

--- Python/Single_manip/get_config.py
+++ Python/Single_manip/get_config.py
@@ -2,17 +2,47 @@ from ncclient import manager
 import xmltodict
 import xml.dom.minidom
 
+# Create an XML filter for targeted NETCONF queries
+netconf_filter = """
+<filter>
+  <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+    <interface></interface>
+  </interfaces>
+</filter>"""
 
+print("Opening NETCONF Connection to {}"
+      .format("host: ATN-IPS-R-192.168.100.146"))
+
+# Open a connection to the network device using ncclient
 with manager.connect(
-        host="192.168.1.20",
+        host="192.168.1.1",
         port=830,
-        username="admin",
+        username="admin", 
         password="nabil",
         hostkey_verify=False
         ) as m:
 
+    print("Sending a <get-config> operation to the device.\n")
+    # Make a NETCONF <get-config> query using the filter
+    netconf_reply = m.get_config(source = 'running', filter = netconf_filter)
 
-        
-    netconf_reply = m.get_config(source = 'running')
-    print (netconf_reply)
+print("Here is the raw XML data returned from the device.\n")
+# Print out the raw XML that returned
+print(xml.dom.minidom.parseString(netconf_reply.xml).toprettyxml())
+print("")
 
+# Parse the returned XML to an Ordered Dictionary
+netconf_data = xmltodict.parse(netconf_reply.xml)["rpc-reply"]["data"]
+
+# Create a list of interfaces
+interfaces = netconf_data["interfaces"]["interface"]
+
+print("The interface status of the device is: ")
+# Loop over interfaces and report status
+for interface in interfaces:
+    print("Interface {} enabled status is {}".format(
+            interface["name"],
+            interface["enabled"]
+            )
+        )
+print("\n")
