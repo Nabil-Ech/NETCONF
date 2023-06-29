b55fb2191634bcd10376aa538cfadfd0e8f12362
Nabil Ech-chokhamny <nabilech01@gmail.com>
2023-06-29 11:48:30 +0200

    29/06

--- Python/Multi_manip/test21.py
+++ Python/Multi_manip/test21.py
@@ -12,7 +12,12 @@ print("Prefix length:", prefix_length)"""
 
 
 def generate_subnets(network, num_ips_list):
-    sorted_ips_list = sorted(num_ips_list, reverse=True) # sorting the list if ip_num
+    devices = {
+    num_ips_router1: "Device1",
+    num_ips_router2: "Device2",
+    num_ips_router3: "Device3"
+    }
+    sorted_ips_list = sorted(num_ips_list, reverse=True)
     subnets = []
     power_list=[]
     power = 0
@@ -32,6 +37,7 @@ def generate_subnets(network, num_ips_list):
                 x=subnet.network_address + L[i], str("/"), subnet_prefix
                 print (subnet.network_address + L[i], subnet_prefix)
                 print (x)
+                print (subnet_prefix)
                 #network = ipaddress.IPv4Network((subnet.network_address + subnet.num_addresses, network.prefixlen))
         elif (power_list[0]>power_list[1] and power_list[1]>power_list[2]):
             for i in range (3):
@@ -62,19 +68,25 @@ def generate_subnets(network, num_ips_list):
         print("This combination is impossible") 
    
 
+"""num_ips_router1 = int(input("Enter the number of IP adresses for Device1: "))
+num_ips_router2 = int(input("Enter the number of IP adresses for Device2: "))
+num_ips_router3 = int(input("Enter the number of IP adresses for Device3: "))"""
 
 
+num_ips_router1 = 100
+num_ips_router2 = 50
+num_ips_router3 = 20
 
 
-    
-
-
-num_ips_router1 = 50
-num_ips_router2 = 40
-num_ips_router3 = 35
-
+#big_network = input("Enter the subnet (X.X.X.X/Y): ")
 
 num_ips_list=[num_ips_router1, num_ips_router2, num_ips_router3]
+
+
+#network = ipaddress.IPv4Network(big_network)
+
 network = ipaddress.IPv4Network("10.10.0.0/24")
+
+
 
 generate_subnets(network, num_ips_list)
